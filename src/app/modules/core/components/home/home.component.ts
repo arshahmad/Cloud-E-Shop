36f366d9a82e8c7235a284e474b027bd0903da52
arshahmad <arshahmadhdi@gmail.com>
2019-06-20 17:06:25 +0530

    observable and class removed

--- src/app/modules/core/components/home/home.component.ts
+++ src/app/modules/core/components/home/home.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ButtonService} from '@ecm-app/button.service';
-import {interval, of, pipe, Subscription, timer} from 'rxjs';
-import {every, map} from 'rxjs/operators';
+
+
 
 @Component ({
   selector: 'ecm-home',
@@ -10,9 +10,7 @@ import {every, map} from 'rxjs/operators';
     'home.component.scss'
   ],
 })
-export class HomeComponent implements OnInit {
-  numberObsSubscription: Subscription;
-  customObsSubscription: Subscription;
+export class HomeComponent {
 
   constructor(private _eventBus: ButtonService) {
   }
@@ -20,45 +18,5 @@ export class HomeComponent implements OnInit {
   toggleDrawer() {
     this._eventBus.broadcast('drawer');
   }
-  ngOnInit(): void {
-//     const myNumbers$ = timer(5000, 1000)
-//       .pipe(map (
-//       (data: number) => {
-// return data * 2;
-//       }
-//     ));
-//     this.numberObsSubscription = myNumbers$.subscribe(
-//       (numbers: number) => {
-//         console.log(numbers);
-//       }
-//     );
-  }
-
-//  const myObservable = Observable.create((observer: Observer <string>) => {
-// setTimeout(() => {
-// observer.next ('you');
-// }, 2000);
-// setTimeout(() => {
-// observer.next ('are');
-// }, 4000);
-// setTimeout(() => {
-// observer.complete ();
-// }, 6000);
-//  });
-//  this.customObsSubscription = myObservable.subscribe(
-//    (data: string) => {
-//      console.log(data);
-//    },
-//  (data: string) => {
-// console.log(data);
-//     },
-//    () => {
-//      console.log('learning');
-//    }
-//  );
-//   }
-//
-//
-//
 }
 
